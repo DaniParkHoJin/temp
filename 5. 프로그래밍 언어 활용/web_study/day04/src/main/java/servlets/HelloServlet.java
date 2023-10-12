786d49c153abe620832b1269be1696b0c06f4208
DaniParkHoJin <parkhojin@naver.com>
2023-10-12 16:01:01 +0900

    10/12 2교시213

--- 5. 프로그래밍 언어 활용/web_study/day04/src/main/java/servlets/HelloServlet.java
+++ 5. 프로그래밍 언어 활용/web_study/day04/src/main/java/servlets/HelloServlet.java
@@ -11,5 +11,6 @@ public class HelloServlet extends HttpServlet {
     @Override
    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("안녕하세요.");
+        System.out.println("djkaljk");
     }
 }
