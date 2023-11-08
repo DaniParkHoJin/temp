576791a74bfec5e55d5fadc41df35dbd0c11a446
DaniParkHoJin <parkhojin@naver.com>
2023-11-08 17:55:23 +0900

    11월8일 수업 후

---
+++ 7. 통합구현/springboot_study/exam01/src/main/java/org/koreait/entities/Member.java
@@ -0,0 +1,27 @@
+package org.koreait.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data @Builder
+@AllArgsConstructor @NoArgsConstructor
+public class Member {
+    @Id
+    private Long userNo;
+    private String userId;
+    @JsonIgnore
+    private String userPw;
+    private String userNm;
+    private String email;
+    private String mobile;
+    @JsonFormat(pattern = "yyyy.MM.dd HH:mm")
+    private LocalDateTime regDt;
+    private LocalDateTime modDt;
+}
